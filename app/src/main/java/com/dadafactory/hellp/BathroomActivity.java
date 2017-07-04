cbab2c4e6082df5189af3337a455e0788de8cd9f
kimalec <kimalec7@gmail.com>
2017-07-04 20:49:24 +0900

    add bathroom activity

--- app/src/main/java/com/dadafactory/hellp/BathroomActivity.java
+++ app/src/main/java/com/dadafactory/hellp/BathroomActivity.java
@@ -1,30 +1,19 @@
 package com.dadafactory.hellp;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    /**
-     *
-     */
-    Button.OnClickListener mClickListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            //이곳에 버튼 클릭시 일어날 일을 적습니다.
-            Intent intent = new Intent(MainActivity.this, BathroomActivity.class);
-            startActivity(intent);
-        }
-    };
+public class BathroomActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        findViewById(R.id.btn_bathroom).setOnClickListener(mClickListener);
+        setContentView(R.layout.activity_bathroom);
     }
 
     @Override
